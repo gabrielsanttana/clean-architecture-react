27bcf67d4c04501665a860c1735638c6e0b7d3b3
Gabriel Santana <gabriel.gsantana7@gmail.com>
2021-05-25 22:22:11 -0300

    refactor: create function to make sut for RemoteAuthentication

--- src/data/usecases/authentication/remoteAuthentication.test.ts
+++ src/data/usecases/authentication/remoteAuthentication.test.ts
@@ -1,17 +1,30 @@
 import {HttpPostClientSpy} from '../../mocks/httpClient';
 import {RemoteAuthentication} from './remoteAuthentication';
 
+// SUT: System Under Test
+
+type SutTypes = {
+  sut: RemoteAuthentication;
+  httpPostClientSpy: HttpPostClientSpy;
+};
+
+const makeSut = (url = 'initial_url'): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy();
+  const sut = new RemoteAuthentication(url, httpPostClientSpy);
+
+  return {
+    sut,
+    httpPostClientSpy,
+  };
+};
+
 describe('The RemoteAuthentication module', () => {
-  it('calls the HTTPClient with correct URL', () => {
+  it('calls the HTTPClient with correct URL', async () => {
     const url = 'https://google.com.br';
-    const httpPostClientSpy = new HttpPostClientSpy();
 
-    const remoteAuthentication = new RemoteAuthentication(
-      url,
-      httpPostClientSpy,
-    );
+    const {sut, httpPostClientSpy} = makeSut(url);
 
-    remoteAuthentication.auth();
+    await sut.auth();
 
     expect(httpPostClientSpy.url).toBe(url);
   });
